3428a8b6121cbef2cbe176db167a17c2c7824dd9
Anupam Maurya <anupammaurya6767@gmail.com>
2022-12-05 16:46:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,6 @@ prev = doc.xpath(
 
 print("running")
 while True:
-    prev_title =  doc.xpath(
-                '//div[@class="container"]/div[@class="row"]//div[@class="col-sm-7"]/div[@class="bigBox"]/div[@class="bigBoxDiv"]/ul[@class="ENABox Events"]/li/a/text()'
-            )[0]
-    
-    prev_link = doc.xpath(
-                '//div[@class="container"]/div[@class="row"]//div[@class="col-sm-7"]/div[@class="bigBox"]/div[@class="bigBoxDiv"]/ul[@class="ENABox Events"]/li/a/@href'
-            )[0]
-    html3 = requests.get("https://www.igdtuw.ac.in/"+prev_link)                                                             
-    doc3 = lxml.html.fromstring(html3.content)                                                                         
-    prev_release_data_link = doc3.xpath('//div[@class="headingPara"]//table[@class="facultyTable"]//a/@href')[0]
-    prev_notice = {                                                                                                         
-                 "title": prev_title,                                                                                                
-                 "link": "https://www.igdtuw.ac.in/" + prev_release_data_link,                                                   
-                 "tab": "Notices/Circulars"                                                                                     
-             } 
-    
-#     url3 = 'http://20.205.15.220/last'
-#     data3 = json.dumps(prev_notice)
-#     x3 = requests.post(url3, json=prev_notice)
-#     print(x3.text)
     try:
         prev_hash = hash(prev)
         new_release = doc.xpath(
@@ -39,32 +19,43 @@ while True:
         )[0]
         new_hash = hash(new_release)
 
-        if (new_hash != prev_hash):
+        if (new_release != prev):
+
+            prev_hash = hash(prev)
+            new_release = doc.xpath(
+                '//div[@class="container"]/div[@class="row"]//div[@class="col-sm-7"]/div[@class="bigBox"]/div[@class="bigBoxDiv"]/ul[@class="ENABox Events"]/li/a'
+            )[0]
+            new_hash = hash(new_release)
             title = doc.xpath(
                 '//div[@class="container"]/div[@class="row"]//div[@class="col-sm-7"]/div[@class="bigBox"]/div[@class="bigBoxDiv"]/ul[@class="ENABox Events"]/li/a/text()'
             )[0]
             link = doc.xpath(
                 '//div[@class="container"]/div[@class="row"]//div[@class="col-sm-7"]/div[@class="bigBox"]/div[@class="bigBoxDiv"]/ul[@class="ENABox Events"]/li/a/@href'
             )[0]
-
-
-            html2 = requests.get("https://www.igdtuw.ac.in/"+link)                                                             
-            doc2 = lxml.html.fromstring(html2.content)                                                                         
-            new_release_data_link = doc2.xpath('//div[@class="headingPara"]//table[@class="facultyTable"]//a/@href')[0]                                                                                              
-            notice = {                                                                                                         
-                 "title": title,                                                                                                
-                 "link": "https://www.igdtuw.ac.in/" + new_release_data_link,                                                   
-                 "tab": "Notices/Circulars"                                                                                     
-            }                                                                                                                  
+            html2 = requests.get("https://www.igdtuw.ac.in/"+link)
+            doc2 = lxml.html.fromstring(html2.content)
+            new_release_data_link = doc2.xpath('//div[@class="headingPara"]//table[@class="facultyTable"]//a/@href')[0]
+            notice = {
+                "title": title,
+                "link": "https://www.igdtuw.ac.in/" + new_release_data_link,
+                "new_link":"https://www.igdtuw.ac.in/" + link,
+                "tab": "Notices/Circulars"
+            }
             url = 'http://20.205.15.220/by'
             data = json.dumps(notice)
-            x = requests.post(url, json=notice)
-            print(x.text)
+            # x = requests.post(url, json=notice)
+            # print(x.text)
             print(notice)
             prev = new_release
 
         else:
             print("no update")
+            prev_hash = hash(prev)
+            new_release = doc.xpath(
+                '//div[@class="container"]/div[@class="row"]//div[@class="col-sm-7"]/div[@class="bigBox"]/div[@class="bigBoxDiv"]/ul[@class="ENABox Events"]/li/a'
+            )[0]
+
+            new_hash = hash(new_release)
 
         time.sleep(10)
         continue
