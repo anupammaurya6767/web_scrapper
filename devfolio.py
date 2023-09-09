ca4307da324af07cf7f93e478cd94d2bbb1f6714
Anupam Maurya <anupammaurya6767@gmail.com>
2023-09-09 08:09:35 +0530

    added db

--- devfolio.py
+++ devfolio.py
@@ -2,10 +2,16 @@ import requests
 import lxml.html
 import time
 import json
+from pymongo import MongoClient
 
-# Initialize an empty list to store previously seen hackathons
-prev_hackathons = []
+# Replace these placeholders with your actual MongoDB URL and database name
+MONGODB_URL = 'YOUR_MONGODB_URL_HERE'
+DATABASE_NAME = 'YOUR_DATABASE_NAME_HERE'
 
+# Initialize MongoDB client and database
+client = MongoClient(MONGODB_URL)
+db = client[DATABASE_NAME]
+hackathons_collection = db['prev_hackathons']
 
 def fetch_hackathons():
     url = 'https://devfolio.co/hackathons'
@@ -16,31 +22,24 @@ def fetch_hackathons():
         '//div[@class="sc-iWajrY sc-bKhNmF hackathons__StyledGrid-sc-fa04e7f9-0  bhEyGM GLpDs"]//div[@class="sc-egNfGp yTbeG CompactHackathonCard__StyledCard-sc-4a10fa2a-0 ihpCnk"]')
 
     hackathons = []
-    #div_element_str = lxml.html.tostring(div_elements, encoding='utf-8', pretty_print=True).decode('utf-8')
-    # print(div_elements)
+
     for div_element in div_elements:
         title_element = div_element.find(
             './/a[@class="Link__LinkBase-sc-af40de1d-0 lkflLS"]//h3[@class="sc-tQuYZ kHbpBI"]')
         title = title_element.text_content() if title_element is not None else ""
 
-
-        #
         link_element = div_element.find('.//a[@class="PillButton-sc-7655a019-0 kTVrbf"]')
         link = link_element.get('href') if link_element is not None else ""
 
-
-        #
         mode_element = div_element.find(
-             './/div[@class="sc-iWajrY deNxdR"]//p[@class="sc-tQuYZ EdbiX"]')
+            './/div[@class="sc-iWajrY deNxdR"]//p[@class="sc-tQuYZ EdbiX"]')
         mode = mode_element.text_content() if mode_element is not None else ""
 
-
-        #
         starts_element = div_element.find(
-             './/div[@class="sc-iWajrY sc-jKDlA-D  fIIKMx"]')
+            './/div[@class="sc-iWajrY sc-jKDlA-D  fIIKMx"]')
         starts_fill = starts_element.text_content() if mode_element is not None else ""
         starts = starts_fill[-8:]
-        if(starts[-5:] != "Ended"):
+        if (starts[-5:] != "Ended"):
             hackathon_info = {
                 "title": title.strip(),
                 "link": link.strip(),
@@ -49,37 +48,35 @@ def fetch_hackathons():
             }
 
             hackathons.append(hackathon_info)
-        #
-        #
-
 
     return hackathons
 
-
 def main():
-    global prev_hackathons  # Access the global list of previously seen hackathons
     print("Running...")
 
     while True:
         try:
-            fetch_hackathons()
             current_hackathons = fetch_hackathons()
 
             # Check for new hackathons
-            new_hackathons = [hackathon for hackathon in current_hackathons if hackathon not in prev_hackathons]
+            new_hackathons = [hackathon for hackathon in current_hackathons if not hackathons_collection.find_one(hackathon)]
 
             if new_hackathons:
                 print("New hackathons found:")
                 for hackathon in new_hackathons:
                     pass
                 print(new_hackathons)
-                # Send new hackathons to the remote endpoint here
-                url3 = 'http://your_endpoint'
-                response = requests.post(url3, json=new_hackathons)
-                print(response.text)
+                # Replace this placeholder with your actual endpoint URL
+                ENDPOINT_URL = 'YOUR_ENDPOINT_URL_HERE'
+                try:
+                    response = requests.post(ENDPOINT_URL, json=new_hackathons)
+                    response.raise_for_status()  # Raise an exception if the request fails (e.g., 4xx or 5xx status codes)
+                    print("Successfully sent new hackathons to the endpoint.")
+                except requests.exceptions.RequestException as request_error:
+                    print("Error sending hackathons to the endpoint:", request_error)
 
-                # Update the list of previously seen hackathons
-                prev_hackathons = current_hackathons
+                # Insert new hackathons into MongoDB
+                hackathons_collection.insert_many(new_hackathons)
 
             else:
                 print("No new updates")
@@ -87,13 +84,13 @@ def main():
         except Exception as e:
             print("Error:", e)
             notice = {"title": "Bot Down", "link": "", "mode": "", "Date": ""}
-            url = 'http://your_endpoint'
-            response = requests.post(url, json=notice)
+            # Replace this placeholder with your actual endpoint URL
+            ENDPOINT_URL = 'YOUR_ENDPOINT_URL_HERE'
+            response = requests.post(ENDPOINT_URL, json=notice)
             print(response.text)
             break
 
         time.sleep(600)  # Sleep for 10 minutes (adjust as needed)
-
 
 if __name__ == "__main__":
     main()
