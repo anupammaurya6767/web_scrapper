7fe689320d2d95260dca1361466cd00121912ef7
Anupam Maurya <anupammaurya6767@gmail.com>
2023-09-10 11:35:55 +0530

    Update and rename libgen.js to libgen.ts

---
+++ Libgen/libgen.ts
@@ -0,0 +1,112 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+async function linkToGet(link: string): Promise<[string, string]> {
+  try {
+    const response = await axios.get(link);
+    const thHtml = cheerio.load(response.data);
+    const tdAll = thHtml('td#info');
+    const tdA = tdAll.find('a');
+    const linkHref = tdA.eq(1).attr('href');
+    const imgLinkTd = tdAll.find('img[alt="cover"]');
+    const imgLinkSrc = imgLinkTd.attr('src');
+    const imgLink = `http://library.lol${imgLinkSrc}`;
+    return [linkHref, imgLink];
+  } catch (error) {
+    console.error('Error in linkToGet:', error);
+    throw error;
+  }
+}
+
+class LibGen {
+  constructor() {
+    // Constructor for LibGen class (if needed)
+  }
+
+  async getBooks(bookName: string): Promise<any[]> {
+    try {
+      const Books: any[] = [];
+      const results = 10;
+      const mainres = 25;
+
+      if (!bookName || bookName.length < 3) {
+        throw new Error('Title Too Short');
+      }
+
+      bookName = bookName.replace(/ /g, '+');
+      const url = `http://libgen.is/search.php?req=${bookName}&lg_topic=libgen&open=0&view=simple&res=${mainres}&phrase=1&column=def`;
+
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+
+      if ($('body').text().includes('Search string must contain minimum 3 characters..')) {
+        throw new Error('Title Too Short');
+      }
+
+      const table = $('table').eq(2);
+      const tableRows = table.find('tr');
+      const objKeys = ['name', 'author', 'size', 'format', 'link', 'language'];
+
+      for (let i = 1, counter = 1; i < tableRows.length && counter <= results; i++) {
+        const row = tableRows.eq(i);
+        const tableDatas = row.find('td');
+        const bookName = tableDatas.eq(2).text();
+        const author = tableDatas.eq(1).text();
+        const link = tableDatas.eq(9).find('a').attr('href');
+        const [linkHref, imgLink] = await linkToGet(link);
+        const language = tableDatas.eq(6).text();
+        const size = tableDatas.eq(7).text();
+        const typeOfIt = tableDatas.eq(8).text();
+
+        if ((typeOfIt !== 'pdf' && typeOfIt !== 'epub') || language !== 'English') {
+          continue;
+        }
+
+        const book = {
+          name: bookName,
+          author: author,
+          size: size,
+          format: typeOfIt,
+          link: linkHref,
+          language: imgLink,
+        };
+
+        Books.push(book);
+        counter++;
+      }
+
+      if (Books.length >= 1) {
+        return Books;
+      } else {
+        throw new Error('No results found');
+      }
+    } catch (error) {
+      console.error('Error in getBooks:', error.message);
+      throw error.message;
+    }
+  }
+}
+
+// Usage
+const libGenScraper = new LibGen();
+const bookName = 'Python'; // Replace with the desired book name
+libGenScraper
+  .getBooks(bookName)
+  .then((bookData) => {
+    if (Array.isArray(bookData)) {
+      bookData.forEach((book) => {
+        console.log('Name:', book.name);
+        console.log('Author:', book.author);
+        console.log('Size:', book.size);
+        console.log('Format:', book.format);
+        console.log('Link:', book.link);
+        console.log('Language:', book.language);
+        console.log();
+      });
+    } else {
+      console.error('Error:', bookData);
+    }
+  })
+  .catch((error) => {
+    console.error('Unknown error occurred:', error);
+  });
