da99c8fba0da545ad1f1ddedbb640d44e01dda74
nancy-singh10~ <nancysingh0526@gmail.com>
2023-10-01 00:58:42 +0530

    added

--- scrape_countries_population/script.py
+++ scrape_countries_population/script.py
@@ -1,18 +1,20 @@
 import pandas as pd
-import requests 
+import requests
 from bs4 import BeautifulSoup
-url="https://www.worldometers.info/world-population/population-by-country"
-response = requests.get(url)
-soup= BeautifulSoup(response.text,'html.parser')
-rows= soup.find("table",{'id':'example2'}).find('tbody').find_all('tr')
-countries_list=[]
-for row in rows:
-  dic={}
-  dic['country']=row.find_all('td')[1].text
-  dic["population "]=row.find_all('td')[2].text.replace(',',',')
 
-#   countries_list.append(dic)
-# print(countries_list[0])
-df=pd.DataFrame(countries_list)
-df.to_excel("countries_data.xlsx",index=False)
-df.to.csv("countries_data.csv",index=False)
+url = "https://www.worldometers.info/world-population/population-by-country"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+rows = soup.find("table", {'id': 'example2'}).find('tbody').find_all('tr')
+countries_list = []
+
+for row in rows:
+    dic = {}
+    dic['country'] = row.find_all('td')[1].text
+    dic["population"] = row.find_all('td')[2].text.replace(',', '')
+
+    countries_list.append(dic)
+
+df = pd.DataFrame(countries_list)
+df.to_excel("countries_data.xlsx", index=False)
+df.to_csv("countries_data.csv", index=False)
