2d80a70df608e502e2c0cecff0bb0e5ce83b2bfc
Kavya Malviya <122686888+KavyaMalviya56@users.noreply.github.com>
2023-10-31 18:52:45 +0530

    Update main.py

--- Github Education/main.py
+++ Github Education/main.py
@@ -3,94 +3,68 @@ from bs4 import BeautifulSoup
 import time
 from pymongo import MongoClient
 
-class Events:
- 
-
-    def __init__(self):
+class EventScraper:
+    def __init__(self, db_uri, db_name, endpoint_url):
         # Initialize MongoDB connection
-        self.client = MongoClient('mongodb://your_username:your_password@localhost:27017')
-        self.db = self.client['your_database_name']
+        self.client = MongoClient(db_uri)
+        self.db = self.client[db_name]
         self.events_collection = self.db['events']
+        self.endpoint_url = endpoint_url
 
     def get_events(self):
-        url = "https://education.github.com/events"
-        events_data = {"events": []}
         try:
+            url = "https://education.github.com/events"
             res = requests.get(url)
             soup = BeautifulSoup(res.text, "html.parser")
 
-            events = soup.find_all("a", class_="d-flex js-landing-page-link event-card")
+            events_data = []
 
-            for e in events:
-                tags_list = []
-                title = e.find("h3", class_="h5").getText().strip()
+            for e in soup.find_all("a", class_="d-flex js-landing-page-link event-card"):
+                title = e.find("h3", class_="h5").get_text(strip=True)
                 img = e.find("img")["src"]
-                try:
-                    desc = (
-                        e.find("p", class_="my-3 short-event color-fg-muted")
-                        .getText()
-                        .strip()
-                    )
-                except:
-                    desc = ""
+                desc = e.find("p", class_="my-3 short-event color-fg-muted").get_text(strip=True, default="")
                 base = e.find_all("p", class_="color-fg-muted text-small")
-                date = base[0].getText().strip()
-                loc = base[1].getText().strip()
-                lang = (
-                    e.find("p", class_="color-fg-muted text-small mb-3")
-                    .getText()
-                    .strip()
-                )
-                labels = e.find_all(
-                    "span", class_="Label--small Label--blue-standard mr-2"
-                )
-                for l in labels:
-                    tags_list.append(l.getText().strip())
+                date, loc = [item.get_text(strip=True) for item in base[:2]]
+                lang = e.find("p", class_="color-fg-muted text-small mb-3").get_text(strip=True)
+                tags_list = [l.get_text(strip=True) for l in e.find_all("span", class_="Label--small Label--blue-standard mr-2")]
                 link = e["href"]
 
-                events_data["events"].append(
-                    {
-                        "title": title,
-                        "image_url": img,
-                        "description": desc,
-                        "date": date,
-                        "location": loc,
-                        "language": lang,
-                        "tags": tags_list,
-                        "link": link,
-                    }
-                )
-            return events_data["events"]
-        except:
-            return None
+                events_data.append({
+                    "title": title,
+                    "image_url": img,
+                    "description": desc,
+                    "date": date,
+                    "location": loc,
+                    "language": lang,
+                    "tags": tags_list,
+                    "link": link,
+                })
+
+            return events_data
+
+        except requests.exceptions.RequestException as request_error:
+            print("Error fetching events:", request_error)
+            return []
 
     def check_and_send_events(self):
-        """
-        Periodically check for new events and send them to an endpoint.
-        """
         while True:
             try:
                 current_events = self.get_events()
-
-                # Check for new events
                 new_events = [event for event in current_events if not self.events_collection.find_one(event)]
 
                 if new_events:
                     print("New events found:")
                     for event in new_events:
-                        pass  # You can perform any specific actions here for new events
+                        pass  # Specific actions for new events can be added here
                     print(new_events)
 
-                    # Replace this placeholder with your actual endpoint URL
-                    ENDPOINT_URL = 'YOUR_ENDPOINT_URL_HERE'
                     try:
-                        response = requests.post(ENDPOINT_URL, json=new_events)
-                        response.raise_for_status()  # Raise an exception if the request fails (e.g., 4xx or 5xx status codes)
+                        response = requests.post(self.endpoint_url, json=new_events)
+                        response.raise_for_status()
                         print("Successfully sent new events to the endpoint.")
                     except requests.exceptions.RequestException as request_error:
                         print("Error sending events to the endpoint:", request_error)
 
-                    # Insert new events into MongoDB
                     self.events_collection.insert_many(new_events)
 
                 else:
@@ -99,14 +73,17 @@ class Events:
             except Exception as e:
                 print("Error:", e)
                 notice = {"title": "Bot Down", "link": "", "mode": "", "Date": ""}
-                # Replace this placeholder with your actual endpoint URL
-                ENDPOINT_URL = 'YOUR_ENDPOINT_URL_HERE'
-                response = requests.post(ENDPOINT_URL, json=notice)
+                response = requests.post(self.endpoint_url, json=notice)
                 print(response.text)
                 break
 
             time.sleep(600)  # Sleep for 10 minutes (adjust as needed)
 
-# Create an instance of the Events class and start checking for new events
-events_instance = Events()
-events_instance.check_and_send_events()
+if __name__ == "__main__":
+    # Replace with your MongoDB URI, database name, and endpoint URL
+    db_uri = 'mongodb://your_username:your_password@localhost:27017'
+    db_name = 'your_database_name'
+    endpoint_url = 'YOUR_ENDPOINT_URL_HERE'
+    
+    event_scraper = EventScraper(db_uri, db_name, endpoint_url)
+    event_scraper.check_and_send_events()
